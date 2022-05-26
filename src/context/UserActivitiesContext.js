5bac2e13d3c9c64dcdbdb9de16ac3be06ec9d133
Mira Tyshchenko <tyshchenkomira@gmail.com>
2022-05-26 23:35:55 +0300

    created new context methods for activities

--- src/context/UserActivitiesContext.js
+++ src/context/UserActivitiesContext.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const UserActivitiesContext = React.createContext();
 
@@ -6,12 +7,26 @@ export default function UserActivitiesProvider({ children }) {
   const [activities, setActivities] = useState([]);
 
   const addActivity = (activity) => {
-    return setActivities((curActivities) => [...curActivities, activity]);
+    const newActivity = { ...activity, id: uuidv4() };
+    return setActivities((curActivities) => [...curActivities, newActivity]);
+  };
+
+  const updateActivities = (newActivities) => {
+    setActivities(newActivities);
+  };
+
+  const deleteActivity = (deleted) => {
+    const newActivities = activities.filter(
+      (activity) => activity.id !== deleted
+    );
+    setActivities(newActivities);
   };
 
   const value = {
     activities,
     addActivity,
+    updateActivities,
+    deleteActivity,
   };
 
   return (
