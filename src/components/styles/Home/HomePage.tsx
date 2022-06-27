25f4741983960ff8fca5bd5e25be3b5e270a36ff
Mira Tyshchenko <tyshchenkomira@gmail.com>
2022-06-27 15:15:19 +0300

    add ts to home page files

---
+++ src/components/styles/Home/HomePage.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { Tooltip } from "@mui/material";
+import {
+  StyledHomeBox,
+  StyledFab,
+} from "./HomePage.styled";
+import { EmptyActivitiesList } from "../Activities/EmptyActivitiestList/EmptyActivitiesList";
+import { StyledAddIcon } from "../Icons/Icons.styled";
+import {
+  StyledModal,
+  StyledModalBlock,
+} from "../Modal/Modal.styled";
+import { AddActivity } from "../../../views/AddActivity";
+import { IActivityItem } from "../../../context/UserActivitiesContext";
+
+interface IHomePageProps {
+  activities: IActivityItem[],
+}
+
+export const HomePage = ({ activities }: IHomePageProps) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
+  return (
+    <StyledHomeBox>
+      {activities.length === 0 && <EmptyActivitiesList />}
+      {activities.length > 0 && (
+        <img
+          src="/home-page.svg"
+          srcSet=""
+          alt=""
+          loading="lazy"
+          style={{ width: 600, height: 700, alignSelf: "center" }}
+        />
+      )}
+      <Tooltip title="Add new activity" onClick={handleOpen}>
+        <StyledFab size="large" color="primary" aria-label="add">
+          <StyledAddIcon />
+        </StyledFab>
+      </Tooltip>
+      <StyledModal open={isOpen} onClose={handleClose}>
+        <StyledModalBlock>
+          <AddActivity onClose={() => setIsOpen(false)} />
+        </StyledModalBlock>
+      </StyledModal>
+    </StyledHomeBox>
+  );
+};
