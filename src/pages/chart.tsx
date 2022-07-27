04948590594e89d1f024e0b2ab9c03cc4e2b91eb
Mira Tyshchenko <tyshchenkomira@gmail.com>
2022-07-27 14:38:31 +0300

    provide SRR for activityItems in chart page

--- src/pages/chart.tsx
+++ src/pages/chart.tsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { getAuth } from "firebase-admin/auth";
+import nookies from "nookies";
+import { loadActivities } from "../context/UserActivitiesContext";
+import firebaseAdmin from "../firebase/firebase.admin";
 import { withPrivate } from "../utils/router";
 import { Diagrams } from "../components/styles/Diagrams/Diagrams";
-import { useActivities } from "../context/UserActivitiesContext";
 
-const Chart = () => {
-  const { activities } = useActivities();
-
-  return <Diagrams activities={activities} />;
+const Chart = ({ activitiesList }) => {
+  return <Diagrams activities={activitiesList} />;
 };
+
+export async function getServerSideProps(ctx) {
+  console.log("[getServerSidePropsList]");
+  try {
+    const cookies = nookies.get(ctx);
+    const user = await getAuth(firebaseAdmin).verifyIdToken(cookies.token);
+    const activitiesList = await loadActivities(user);
+    console.log("activities; ", activitiesList);
+  
+    return { props: { activitiesList: JSON.parse(JSON.stringify(activitiesList)) } };
+  } catch (e) {
+    return { props: {} as never };
+  }
+}
 
 export default withPrivate(Chart);
