1253d8a45492ca492feb5cc17c3deb8ccb1e91b5
Mira Tyshchenko <tyshchenkomira@gmail.com>
2022-07-27 14:38:08 +0300

    provide SRR for activityItems in scheduler page

--- src/pages/scheduler.tsx
+++ src/pages/scheduler.tsx
@@ -1,19 +1,37 @@
 import React from "react";
-import { withPrivate } from "../utils/router";
-import { Calendar } from "../components/styles/Calendar/Calendar";
-import { useActivities } from "../context/UserActivitiesContext";
+import { getAuth } from "firebase-admin/auth";
+import nookies from "nookies";
+import firebaseAdmin from "../firebase/firebase.admin";
 
-const Scheduler = () => {
-  const { activities, updateActivities, deleteActivity, addActivity } =
+import { withPrivate } from "../utils/router";
+import { loadActivities, useActivities } from "../context/UserActivitiesContext";
+
+import { Calendar } from "../components/styles/Calendar/Calendar";
+
+const Scheduler = ({ activitiesList }) => {
+  const { updateActivities, deleteActivity, addActivity } =
     useActivities();
 
   return (
     <Calendar
-      activities={activities}
+      activities={activitiesList}
       updateActivities={updateActivities}
       deleteActivity={deleteActivity}
       addActivity={addActivity}/>
   );
 };
+
+export async function getServerSideProps(ctx) {
+  try {
+    const cookies = nookies.get(ctx);
+    const user = await getAuth(firebaseAdmin).verifyIdToken(cookies.token);
+    const activitiesList = await loadActivities(user);
+    console.log("activities; ", activitiesList);
+  
+    return { props: { activitiesList: JSON.parse(JSON.stringify(activitiesList)) } };
+  } catch (e) {
+    return { props: {} as never };
+  }
+}
 
 export default withPrivate(Scheduler);
